9427f78812ace7d1df2bc9bd074cfafea86b322c
UnknownName01 <87627142+Unname10@users.noreply.github.com>
2024-04-03 16:24:41 +0700

    (init) Khởi tạo Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+
+import LeftPanel from './components/LeftPanel';
+import './App.scss';
+
+function App() {
+	return (
+		<Container fluid>
+			<LeftPanel />
+		</Container>
+	);
+}
+
+export default App;
---
+++ src/components/LeftPanel/LeftPanel.jsx
@@ -0,0 +1,15 @@
+import { Col, Row } from 'react-bootstrap';
+
+function LeftPanel() {
+	return (
+		<Col lg={6}>
+			<Row>
+				<Col>
+					<p className=''>Base64Converter</p>
+				</Col>
+			</Row>
+		</Col>
+	);
+}
+
+export default LeftPanel;
